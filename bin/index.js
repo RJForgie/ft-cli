7b7c6103d034a4984901eeebb30280d8da36e269
Ryan <ryan.j.forgie@gmail.com>
2020-08-19 19:18:34 +0100

    log out headlines

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,36 @@
 #!/usr/bin/env node
 
-console.log( "Hello!" );
+const axios = require("axios");
+const cheerio = require('cheerio');
+
+
+console.log("Wall Street Jounral headlines:");
+
+const url = "https://www.ft.com/news-feed/";
+
+const logHeadline = (headline) => {
+  const $ = cheerio.load(headline.html);
+
+  $('.js-teaser-heading-link').each(( i, element) => {
+     console.log($(element).text())
+  })
+  
+  $(".js-teaser-standfirst-link").each((i, element) => {
+    console.log($(element).text());
+  });
+
+  console.log("-");
+}
+
+const handleResponse = (headlines) => {
+  headlines.forEach(headline => {
+    logHeadline(headline)
+  });
+}
+
+axios.get(url, { headers: { Accept: "application/json" } })
+ .then(response => {
+  handleResponse(response.data.items)
+ });
+
+

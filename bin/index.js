f91ff2aa7feaa9bbe6e86ca4aa9b4f09e506d2d8
Ryan <ryan.j.forgie@gmail.com>
2020-08-21 08:25:55 +0100

    add argument handling

--- bin/index.js
+++ bin/index.js
@@ -2,35 +2,38 @@
 
 const axios = require("axios");
 const cheerio = require('cheerio');
-
-
-console.log("Wall Street Jounral headlines:");
-
-const url = "https://www.ft.com/news-feed/";
+const { argv } = require('yargs')
 
 const logHeadline = (headline) => {
   const $ = cheerio.load(headline.html);
-
+  
   $('.js-teaser-heading-link').each(( i, element) => {
-     console.log($(element).text())
+    console.log($(element).text())
   })
   
   $(".js-teaser-standfirst-link").each((i, element) => {
     console.log($(element).text());
   });
-
+  
   console.log("-");
 }
 
-const handleResponse = (headlines) => {
-  headlines.forEach(headline => {
-    logHeadline(headline)
-  });
+const args = () => argv.number
+
+const handleResponse = (headlines, numberToOutput) => {
+  for(let i = 0; i < numberToOutput; i++) {
+    logHeadline(headlines[i])
+  }
 }
 
+const url = "https://www.ft.com/news-feed/";
+
 axios.get(url, { headers: { Accept: "application/json" } })
- .then(response => {
-  handleResponse(response.data.items)
+.then(response => {
+  const numberToOutput = args() 
+  console.log("Wall Street Journal headlines:");
+  handleResponse(response.data.items, numberToOutput)
+  console.log("Full articles available here: https://www.ft.com/news-feed");
  });
 
 

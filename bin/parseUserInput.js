132988c39eb7e9c2fc5b651857007c8ce539372f
Ryan <ryan.j.forgie@gmail.com>
2020-08-24 21:47:06 +0100

    message when user enters incorrect args

--- bin/parseUserInput.js
+++ bin/parseUserInput.js
@@ -1,9 +1,10 @@
-const parseUserInput = (userInput) => {
-    if (userInput && userInput <= 25 ) {
-      return userInput
-    } else {
-      return 5
-    }
+const parseUserInput = userInput => {
+  if (userInput && userInput <= 25) {
+    console.log("Invalid user input, returning 5 headlines");
+    return userInput;
+  } else {
+    return 5;
   }
+};
 
-  exports.parseUserInput = parseUserInput
+exports.parseUserInput = parseUserInput;
